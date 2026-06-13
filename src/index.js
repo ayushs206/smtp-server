8f7cf90f3f71b64dc73f41c18a1cbe0647726512
Ayush <ayushsingla206@gmail.com>
2026-06-13 23:23:51 +0530

    Initialize SMTP server project with essential files and configurations

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "./envLoad.js";
+
+import { SMTPServer } from "smtp-server";
+
+const smtpServer = new SMTPServer({
+    allowInsecureAuth: true,
+    authOptional: true,
+    onConnect(session, callback) {
+        console.log("Client connected:", session.remoteAddress);
+        callback();
+    },
+    onMailFrom(address, session, callback) {
+        console.log("MAIL FROM:", address.address);
+        callback();
+    },
+    onRcptTo(address, session, callback) {
+        console.log("RCPT TO:", address.address);
+        callback();
+    },
+    onData(stream, session, callback) {
+        let emailData = "";
+        stream.on("data", (chunk) => {
+            emailData += chunk.toString();
+        });
+        stream.on("end", () => {
+            console.log("Email received:\n", emailData);
+            callback();
+        });
+    }
+});
+
+smtpServer.listen(process.env.SMTP_PORT, () => {
+    console.log(`SMTP server is listening on port ${process.env.SMTP_PORT}`);
+});
